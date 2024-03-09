56af25abca3fc02c1c3393d51161a8f4a183a152
Antonin Data <antonin.datas@gmail.com>
2024-03-09 19:54:38 +0100

    extension classifier

--- Classifier.py
+++ Classifier.py
@@ -2,6 +2,7 @@ import pandas as pd
 import math as m
 import graphviz
 import os
+import random as rd
 
 def value_obs(observation):
     return (observation == "P").sum()/len(observation)
@@ -11,13 +12,33 @@ def entropy(observation):
         return 0
     return - p * m.log(p,2) - (1-p) * m.log(1-p,2)
 class Node:
-    def __init__(self, entropy=0, value=None, left_son=None, right_son=None, name=None, edge_label=None):
+    def __init__(self, entropy=0, value=None, left_son=None, right_son=None, name=None, edge_label=None, sub_entropy=0, nb_leaves=1):
         self.entropy = entropy
         self.value = value
         self.name = name
         self.left_son = left_son
         self.right_son = right_son
         self.edge_label = edge_label # for graphviz, label on the edge with its parent
+        self.sub_entropy = sub_entropy
+        self.nb_leaves = nb_leaves
+
+    def copy(self):
+        if self.nb_leaves == 1:
+            return Node(self.e,self.value,None,None,self.name,self.edge_label,self.sub_entropy,self.nb_leaves)
+        else:
+            l,r = self.copy(self.left_son), self.copy(self.right_son)
+            return Node(self.e,self.value,l,r,self.name,self.edge_label,self.sub_entropy,self.nb_leaves)
+
+    def copy_until(self,i,c):
+        """ copy all the tree except the subnode with root at index i """
+        if c == i or self.nb_leaves == 1:
+            return Node(self.e,self.value,None,None,self.name,self.edge_label,self.sub_entropy,self.nb_leaves)
+
+
+
+
+
+
 class ClassifierTree:
     def __init__(self, min_obs=2, max_depth=None):
         self.root = None
@@ -33,7 +54,7 @@ class ClassifierTree:
         e = entropy(y)
         # Stopping criterions : Too deep, too few obs or pure/impure
         if depth >= self.max_depth or len(y) <= self.min_obs or p == 1 or p == 0:
-            return Node(e,p,None,None,"Leaf",label)
+            return Node(e,p,None,None,"Leaf",label,e,1)
         # Else binary split to maximize info gain
         max_info = -float("inf")
         index = None
@@ -53,9 +74,23 @@ class ClassifierTree:
             y_l, y_r = y[x[index] == res_l], y[x[index] == res_r]
             left_son = self.rec_build_tree(x_l,y_l,depth+1,res_l)
             right_son = self.rec_build_tree(x_r,y_r,depth+1,res_r)
-            return Node(e,p,left_son,right_son,index,label)
+            sub_entropy = left_son.sub_entropy+right_son.sub_entropy
+            nb_leaves = left_son.nb_leaves + right_son.nb_leaves
+            return Node(e,p,left_son,right_son,index,label,sub_entropy,nb_leaves)
         else :
-            return Node(e,p,None,None,"Leaf",None)
+            # Else the algo stop and we get a leaf (cf pure with respect to other features)
+            return Node(e,p,None,None,"Leaf",label,e,1)
+
+
+    def CCP(self):
+        """ Naive algo that computes alphas and subtrees associated using Cost Complexity Pruning Algo """
+        alphas, subtrees = [],[]
+        self.CCP_rec(self.root)
+
+    def CCP_rec(self,node,alphas,subtrees):
+        if node.nb_leaves == 1:
+            # leaf
+            pass
 
     def display(self):
         dot = graphviz.Digraph(comment="Classifier")
@@ -66,7 +101,7 @@ class ClassifierTree:
     def display_rec(self,dot,node,nb,nb_parent):
         if node != None:
             # necesseraly two sons
-            dot.node(str(nb), node.name + "\nvalue : " + str(node.value) + "\nEntropy : " + str(node.entropy))
+            dot.node(str(nb), node.name + "\nvalue : " + str(node.value) + "\nEntropy : " + str(node.entropy) + "\nSub Entropy : " + str(node.sub_entropy) + "\nNb Leaves : " + str(node.nb_leaves))
             if nb != 0:
                 # if not root, edge from its parent
                 dot.edge(str(nb_parent),str(nb), label=node.edge_label)
@@ -74,10 +109,11 @@ class ClassifierTree:
             self.display_rec(dot,node.right_son,2*nb+2,nb)
 
 def main():
-    data = pd.DataFrame({'Sex':["M","M","F","F","M","F"],
-                         'Study':["W","NW","W","W","NW","NW"],
-                         'Courses':["O","NO","O","NO","NO","NO"],
-                         'Result':["P","F","P","P","P","F"]})
+    rd.seed(1) # for reproductibility
+    data = pd.DataFrame({'Sex': rd.choices(["M","F"],k=100),
+                         'Study': rd.choices(["W","NW"],k=100),
+                         'Courses':rd.choices(["O","NO"],k=100),
+                         'Result':rd.choices(["P","F"],k=100)})
     # Entropy test
     Y = data.iloc[:,-1]
     print(entropy(Y))
@@ -98,5 +134,8 @@ def main():
     classifier = ClassifierTree(1,3)
     classifier.build_tree(data)
     classifier.display()
+
+    # Cost Complexity Pruning
+    # interval of values for alpha (pruning parameter)
 
 main()
